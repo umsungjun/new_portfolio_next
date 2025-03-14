f21f23d5439b9e543d2717882482e07fd439eeae
umsungjun <umseongjun@naver.com>
2025-03-14 21:20:42 +0900

    Refactor: i18n 지원하는 언어, 기본 언어를 전역 상수로 교체

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,11 +1,12 @@
 import { defineRouting } from "next-intl/routing";
 import { createNavigation } from "next-intl/navigation";
+import { LOCALE_KO, SUPPORTED_LOCALES } from "@/lib/constants";
 
 export const routing = defineRouting({
   /* 지원하는 언어 설정 */
-  locales: ["ko", "en"],
+  locales: SUPPORTED_LOCALES,
   /* 기본 언어 설정 */
-  defaultLocale: "ko",
+  defaultLocale: LOCALE_KO,
 });
 
 /* export const { Link, redirect, usePathname, useRouter } = createNavigation(routing); 코드는 Next.js의 Link, redirect, usePathname, useRouter를 사용할 수 있도록 래핑한 것입니다. 이를 사용하여 기존 next j에서 제공하는 API를 대체할 수 있습니다. */
---
+++ lib/constants.ts
@@ -0,0 +1,4 @@
+/* 언어 관련 전역 상수 */
+export const LOCALE_KO = "ko";
+export const LOCALE_EN = "en";
+export const SUPPORTED_LOCALES = [LOCALE_KO, LOCALE_EN];
