77a4812777e70ca9e394c29cbdea8c72eb9b980a
umsungjun <umseongjun@naver.com>
2025-03-15 16:33:19 +0900

    Feat: next-intl 미들웨어 설정 및 로케일 리다이렉션 추가
    - next-intl 미들웨어를 사용하여 로케일 기반 라우팅 설정
    - 지원되지 않는 로케일로 접근 시 기본 로케일(ko)로 리다이렉션
    - 특정 경로에 대해 미들웨어를 실행하도록 matcher 설정

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,26 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
+import { NextRequest, NextResponse } from "next/server";
+import { LOCALE_KO, SUPPORTED_LOCALES } from "./lib/constants";
 
 export default createMiddleware(routing);
 
+export const middleware = async (request: NextRequest) => {
+  const pathname = request.nextUrl.pathname;
+  const locale = pathname.split("/")[1];
+
+  if (!SUPPORTED_LOCALES.includes(locale)) {
+    const url = new URL(`/${LOCALE_KO}`, request.url);
+    return NextResponse.redirect(url);
+  }
+};
+
 export const config = {
-  /* 지워하는 언어만 라우팅 */
-  matcher: ["/", "/(ko|en)/:path*"],
+  /* middleware를 실행시킬 페이지 
+    "/" 꼭 붙여야 함
+    /user/:path* <- user path로 시작하는 곳에 전부 적용
+    matcher: ["/", "/profile", "/create-account", "/user/:path*"]
+  */
+
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
