fd3c8636d2730a4ad355f2d092c14ace1d389f37
umsungjun <umseongjun@naver.com>
2025-03-19 19:29:59 +0900

    Refactor: Feat: 메인 페이지 우측 채팅 화면 home/@chat 폴더 하위로 이동
    - home 라우팅 경로에서 parallel routes를 활용하여 병렬 라우트 처리하기 위함

---
+++ app/[locale]/home/@chat/page.tsx
@@ -0,0 +1,11 @@
+import ChatBody from "./body";
+import ChatHeader from "./header";
+
+export default async function Chat() {
+  return (
+    <section className="max-w-[475px] w-[475px] h-dvh border border-solid border-y-0 border-slate-200 bg-gradient-to-r from-[#d7e5fa] via-[#ede9f8] to-[#d7e5fa] bg-[length:200%_100%] animate-gradientAnimation">
+      <ChatHeader />
+      <ChatBody />
+    </section>
+  );
+}
