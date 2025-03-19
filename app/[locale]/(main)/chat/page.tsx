35862b6b4cf46513bf107d58f61c9e2d9689fc20
umsungjun <umseongjun@naver.com>
2025-03-19 19:31:20 +0900

    Remove: 기존 메인 페이지 우측 채팅 화면관련 컴포넌트 제거

--- app/[locale]/(main)/chat/page.tsx
+++
@@ -1,9 +0,0 @@
-import ChatHeader from "./header";
-
-export default async function Chat() {
-  return (
-    <section className="max-w-[475px] w-[475px] h-dvh border border-solid border-y-0 border-slate-200 bg-gradient-to-r from-[#d7e5fa] via-[#ede9f8] to-[#d7e5fa] bg-[length:200%_100%] animate-gradientAnimation">
-      <ChatHeader />
-    </section>
-  );
-}
