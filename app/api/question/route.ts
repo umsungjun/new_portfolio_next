622dbb65b6d2bce03e806bdf6930658804cbe2d7
umsungjun <umseongjun@naver.com>
2025-03-23 19:05:57 +0900

    Refactor: 질문 데이터를 가져오는 API 수정
    - 데이터가 없을 경우 404 상태 코드와 함께 에러 메시지 반환
    - 서버 오류 발생 시 500 상태 코드와 함께 에러 메시지 반환

--- app/api/question/route.ts
+++ app/api/question/route.ts
@@ -4,14 +4,23 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const data = await prisma.question.findMany({
-      select: {
-        id: true,
-        content: true,
+      orderBy: {
+        id: "asc",
       },
     });
 
+    if (!data || data.length === 0) {
+      return NextResponse.json(
+        { success: false, message: "데이터가 존재하지 않습니다." },
+        { status: 404 }
+      );
+    }
+
     return NextResponse.json({ success: true, data });
   } catch (error) {
-    return NextResponse.json({ success: false, error }, { status: 500 });
+    return NextResponse.json(
+      { success: false, message: `서버 내부 오류가 발생했습니다. ${error}` },
+      { status: 500 }
+    );
   }
 }
