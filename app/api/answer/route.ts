25bd976226c7869c76e780b0a7d994338b69666d
umsungjun <umseongjun@naver.com>
2025-03-23 19:07:48 +0900

    Feat: 특정 답변 데이터를 조회하는 POST API 추가
    - /api/answer 경로에서 POST 요청을 통해 특정 id의 답변 데이터를 조회
    - request body id 값을 추출하고 유효성 검사 추가
    - Prisma를 사용하여 데이터베이스에서 id 값에 해당하는 답변 데이터 조회
    - 잘못된 id 값일 경우 400 상태 코드와 함께 에러 메시지 반환
    - 서버 오류 발생 시 500 상태 코드와 함께 에러 메시지 반환

---
+++ app/api/answer/route.ts
@@ -0,0 +1,27 @@
+import { prisma } from "@/lib/server/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const { id } = body;
+
+    if (!id || isNaN(Number(id))) {
+      return NextResponse.json(
+        { success: false, message: "잘못된 ID 값입니다." },
+        { status: 400 }
+      );
+    }
+
+    const data = await prisma.answer.findUnique({
+      where: { id: Number(id) },
+    });
+
+    return NextResponse.json({ success: true, data });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: `서버 내부 오류가 발생했습니다. ${error}` },
+      { status: 500 }
+    );
+  }
+}
