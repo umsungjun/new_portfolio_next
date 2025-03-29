885a273466edd7f1fe4d23570c26df12d9e2d520
umsungjun <umseongjun@naver.com>
2025-03-29 12:09:06 +0900

    Refator: Question 컴포넌트를 Answer 컴포넌트와 통일성 있게 리팩토링

--- app/[locale]/home/@chat/_component/question.tsx
+++ app/[locale]/home/@chat/_component/question.tsx
@@ -1,13 +1,21 @@
+import { LOCALE_KO } from "@/lib/client/constants";
+import { Question as QuestionType } from "@prisma/client";
+
 interface QuestionProps {
   showMark?: boolean;
-  text: string;
+  locale: string;
+  chat: QuestionType;
 }
 
-export default function Question({ showMark = true, text }: QuestionProps) {
+export default function Question({
+  showMark = true,
+  locale,
+  chat,
+}: QuestionProps) {
   return (
     <div className="questionWrapper">
       {showMark && <div className="questionMark">Q</div>}
-      <p>{text}</p>
+      <p>{locale === LOCALE_KO ? chat.contentKo : chat.contentEn}</p>
     </div>
   );
 }
