b92c295de3455e9f9216cb8daff1603707c85ace
umsungjun <umseongjun@naver.com>
2025-03-29 15:50:05 +0900

    Feat: Answer 컴포넌트 생성
    - 답변 텍스트를 언어(locale)에 따라 한국어 또는 영어로 표시
    - mediaType이 IMAGE일 경우 참고 이미지를 렌더링하도록 로직 추가
    - mediaType이 VIDEO일 경우 참고 영상을 렌더링하도록 로직 추가
    - YoutubePlayer를 사용하여 영상 재생 기능 구현
    - PulseLoader를 사용하여 로딩 상태를 표시하는 스피너 추가

---
+++ app/[locale]/home/@chat/_component/answer.tsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import { useTranslations } from "next-intl";
+import PulseLoader from "react-spinners/PulseLoader";
+import YoutubePlayer from "react-player";
+import { Answer as AnswerType } from "@prisma/client";
+import { LOCALE_KO } from "@/lib/client/constants";
+
+import { PhotoIcon, VideoCameraIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+
+interface AnswerProps {
+  isRefresh: boolean;
+  locale: string;
+  chat: AnswerType;
+}
+
+const IMAGE_TYPE = "IMAGE";
+const VIDEO_TYPE = "VIDEO";
+
+export default function Answer({ isRefresh, locale, chat }: AnswerProps) {
+  const t = useTranslations();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
+  return (
+    <>
+      {!isRefresh && isLoading ? (
+        <div className="answerLoader">
+          <PulseLoader
+            color="#3B82f6"
+            size={10}
+            loading={true}
+            speedMultiplier={0.8}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <div className="answer">
+          <p className="whitespace-pre-line">
+            <span className="font-black text-blue-600 pr-1">A.</span>
+            {locale === LOCALE_KO ? chat.contentKo : chat.contentEn}
+          </p>
+          {/* 참고 이미지 */}
+          {chat.mediaType === IMAGE_TYPE && chat.mediaUrl !== null && (
+            <div>
+              <span className="flex items-center gap-1 font-bold">
+                <PhotoIcon className="size-5 text-green-500" />
+                {t("referenceImage")}
+              </span>
+              <div className="relative mt-3 flex h-[200px] w-full">
+                <Image
+                  fill
+                  src={`${process.env.NEXT_PUBLIC_GOOGLE_DRIVE_IMG_URL}${chat.mediaUrl}&sz=w300`}
+                  priority={true}
+                  alt="referenceImage"
+                  sizes="(max-width: 215px) 100vw"
+                />
+              </div>
+            </div>
+          )}
+          {/* 참고 영상 */}
+          {chat.mediaType === VIDEO_TYPE && chat.mediaUrl !== null && (
+            <div className="mt-3 flex flex-col gap-3">
+              <span className="flex items-center gap-1 font-bold">
+                <VideoCameraIcon className="size-5 text-red-500" />
+                {t("referenceVideo")}
+              </span>
+              <div className="videoPlayer flex justify-center ">
+                <YoutubePlayer url={chat.mediaUrl} />
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+    </>
+  );
+}
